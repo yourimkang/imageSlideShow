5e6d4733abf051def2f7c6618ce99834e854ad4f
Yurim Kang <kyr9909@naver.com>
2016-05-26 20:16:38 +0900

    #2 Event Programming

--- app/src/main/java/kr/hs/emirim/kyr9909/imageslideshow/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/kyr9909/imageslideshow/MainActivity.java
@@ -2,12 +2,36 @@ package kr.hs.emirim.kyr9909.imageslideshow;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ViewFlipper;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    ViewFlipper flip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        flip = (ViewFlipper)findViewById(R.id.view_flip);
+        flip.setFlipInterval(1000);
+        Button butStart=(Button)findViewById(R.id.but_start);
+        butStart.setOnClickListener(this);
+        Button butStop=(Button)findViewById(R.id.but_stop);
+        butStart.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId())
+        {
+            case R.id.but_start:
+                flip.startFlipping();
+                break;
+            case R.id.but_stop:
+                flip.stopFlipping();
+                break;
+
+        }
     }
 }
