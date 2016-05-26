12504419a63f05cd6f33cc8efcac7c5e0aecd84a
Yurim Kang <kyr9909@naver.com>
2016-05-26 20:57:11 +0900

    #3 EditText

--- app/src/main/java/kr/hs/emirim/kyr9909/imageslideshow/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/kyr9909/imageslideshow/MainActivity.java
@@ -4,17 +4,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     ViewFlipper flip;
+    EditText editTime;
+    int time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         flip = (ViewFlipper)findViewById(R.id.view_flip);
-        flip.setFlipInterval(1000);
+        editTime=(EditText)findViewById(R.id.edit_time);
         Button butStart=(Button)findViewById(R.id.but_start);
         butStart.setOnClickListener(this);
         Button butStop=(Button)findViewById(R.id.but_stop);
@@ -26,6 +29,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch(v.getId())
         {
             case R.id.but_start:
+                time=(int)(Double.parseDouble(editTime.getText().toString())*1000);
+                flip.setFlipInterval(time);
                 flip.startFlipping();
                 break;
             case R.id.but_stop:
